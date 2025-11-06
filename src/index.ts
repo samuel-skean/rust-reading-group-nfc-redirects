3c713df3c5ffb6786efb62885da7ccfa05930fbc
Samuel Skean <sskean2@uic.edu>
2025-11-06 07:59:40 -0600

    Initial static redirect.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.redirect("https://rust-unofficial.github.io/too-many-lists/");
 });
 
 export default app;
